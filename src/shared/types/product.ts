b01b7855c2056f64f6c6a27dfc6dcc77a339866a
Vinicius <viniciusaugustooliveira2007@gmail.com>
2025-10-29 12:04:50 -0300

    refactor: product option types to use Prisma models
    - Replaced custom OptionValue and ProductOption interfaces with types based on Prisma's ProductOption and ProductOptionValue.

--- src/shared/types/product.ts
+++ src/shared/types/product.ts
@@ -1,16 +1,10 @@
+import type { ProductOption, ProductOptionValue } from "@prisma/client";
 import type { ProductInclude } from "./Includes";
 
 export type ProductOptionsArray = ProductInclude["productOption"];
 
-export type OptionValue = {
-  id: number;
-  value: string;
-};
+export type OptionValue = ProductOptionValue;
 
-export interface ProductOption {
-  id: number;
-  type: string;
+export type ProductOption1 = ProductOption & {
   values: OptionValue[];
-}
-
-
+};
